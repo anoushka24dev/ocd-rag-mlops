f371cc45f3428341df8258e8df2716e376a211fa
anoushka <anoushka242006@gmail.com>
2026-04-23 21:37:08 +0530

    Add Docker deployment files

---
+++ api.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from engine import process_query
+
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.llms import Ollama
+from langchain.chains import RetrievalQA
+
+app = FastAPI()
+
+
+# ---------- Load once at startup ----------
+
+embedding_model = HuggingFaceEmbeddings(
+    model_name="all-MiniLM-L6-v2"
+)
+
+vector_db = Chroma(
+    persist_directory="chroma_db",
+    embedding_function=embedding_model
+)
+
+retriever = vector_db.as_retriever(search_kwargs={"k":5})
+
+llm = Ollama(model="mistral")
+
+qa_chain = RetrievalQA.from_chain_type(
+    llm=llm,
+    retriever=retriever,
+    return_source_documents=True
+)
+
+memory = {}
+
+
+# ---------- API route ----------
+
+@app.post("/chat")
+def chat(query: str):
+
+    response = process_query(
+        query,
+        memory,
+        qa_chain
+    )
+
+    return {
+        "response": response
+    }
